8df582b173b1bcf853970341187eb94cc7e42d93
anuragpratap05 <56753626+anuragpratap05@users.noreply.github.com>
2020-03-05 14:19:24 +0530

    Create infttq.py

---
+++ infttq.py
@@ -0,0 +1,26 @@
+def validator(s):
+    stack=[]
+    count=0
+    for i in s:
+        if(i=='[' or i=='{' or i=='('):
+            stack.append(i)
+            count+=1
+            continue
+        if(len(stack)==0):
+            return count+1
+        x=stack.pop()
+        if(i==']' and x=='['):
+            count+=1
+        elif(i=='}' and x=='{'):
+            count+=1
+        elif(i==')' and x=='('):
+            count+=1
+        else:
+            return count+1
+    if(len(stack)==0):
+        return 0
+    else:
+        return count+1
+s=input()
+print(validator(s))
+        
